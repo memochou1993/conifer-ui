3fec3541108d44ffc98ba37bc0e55cd4c7e2cac7
Memo Chou <memochou1993@gmail.com>
2022-10-22 01:23:32 +0800

    Fetch records

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+use gloo_net::http::Request;
+use serde::Deserialize;
+use std::time::SystemTime;
+use wasm_bindgen_futures::spawn_local;
+use yew::prelude::*;
+
+#[derive(Deserialize)]
+struct Response {
+    data: Vec<Record>,
+}
+
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+struct Record {
+    id: String,
+    url: String,
+    expired_at: SystemTime,
+    updated_at: SystemTime,
+    created_at: SystemTime,
+}
+
+#[derive(PartialEq, Properties)]
+struct RecordListProps {
+    records: Vec<Record>,
+    on_click: Callback<Record>,
+}
+
+#[function_component(RecordList)]
+fn record_list(RecordListProps { records, on_click }: &RecordListProps) -> Html {
+    records
+        .iter()
+        .map(|record| {
+            html! {
+                <p
+                    onclick={
+                        let on_click = on_click.clone();
+                        let record = record.clone();
+                        Callback::from(move |_| on_click.emit(record.clone()))
+                    }
+                    style="cursor: pointer;"
+                >
+                    {format!("http://127.0.0.1:8000/{}", record.id)}
+                </p>
+            }
+        })
+        .collect()
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    let records = use_state(|| vec![]);
+
+    {
+        let records = records.clone();
+        use_effect_with_deps(
+            move |_| {
+                let records = records.clone();
+                spawn_local(async move {
+                    let res: Response = Request::get("http://127.0.0.1:8000/api/records")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    records.set(res.data);
+                });
+                || ()
+            },
+            (),
+        );
+    }
+
+    let on_record_click = {
+        Callback::from(move |record: Record| {
+            log::info!("{:#?}", record);
+        })
+    };
+
+    html! {
+        <div>
+            <RecordList
+                records={(*records).clone()}
+                on_click={on_record_click}
+            />
+        </div>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use conifer_ui::App;
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
 }
